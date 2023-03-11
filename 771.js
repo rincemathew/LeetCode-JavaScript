3e2f07168fef7f1aaa5fe7433130bd8d099b2bca
Rince Mathew <rincemathewmanattu@gmail.com>
2023-03-11 22:38:02 +0530

    added 771

---
+++ 771.js
@@ -0,0 +1,8 @@
+// Jewels and Stones
+var numJewelsInStones = function(jewels, stones) {
+    count = 0;
+    for(i=0;i<jewels.length;i++){
+        count += stones.split(jewels[i]).length-1
+    }
+    return count
+};
