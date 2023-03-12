03b6218d1b592466a0ec4bf962b43a97234914a1
Rince Mathew <rincemathewmanattu@gmail.com>
2023-03-12 16:23:29 +0530

    added1470

---
+++ 1470.js
@@ -0,0 +1,10 @@
+var shuffle = function(nums, n) {
+    result = []
+    firstArray = nums.slice(0,n)
+    secondArray = nums.slice(n,n+n)
+    for(i=0;i<n;i++){
+        result.push(firstArray[i])
+        result.push(secondArray[i])
+    }
+    return result
+};
