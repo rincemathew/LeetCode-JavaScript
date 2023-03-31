9a931180e88170c80b865ae8164e64ea1bc4ad55
Rince Mathew <rincemathewmanattu@gmail.com>
2023-03-31 20:17:37 +0530

    to lower case

---
+++ 709.js
@@ -0,0 +1,13 @@
+// To Lower Case
+
+// Given a string s, return the string after replacing every uppercase letter with the same lowercase letter.
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var toLowerCase = function(s) {
+    return s.toLowerCase();
+};
+
+console.log(toLowerCase("Hello"))
