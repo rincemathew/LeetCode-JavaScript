0ea71f8096e643336f6de2c382e6f43f57314fc4
Rince Mathew <rincemathewmanattu@gmail.com>
2023-04-07 18:05:40 +0530

    truncate sentence

---
+++ 1816.js
@@ -0,0 +1,20 @@
+// Truncate Sentence
+
+// A sentence is a list of words that are separated by a single space with no leading or trailing spaces. Each of the words consists of only uppercase and lowercase English letters (no punctuation).
+
+// For example, "Hello World", "HELLO", and "hello world hello world" are all sentences.
+// You are given a sentence s​​​​​​ and an integer k​​​​​​. You want to truncate s​​​​​​ such that it contains only the first k​​​​​​ words. Return s​​​​​​ after truncating it.
+
+/**
+ * @param {string} s
+ * @param {number} k
+ * @return {string}
+ */
+var truncateSentence = function(s, k) {
+    return s.split(' ').slice(0,k).join(' ')
+};
+
+
+console.log(truncateSentence("Hello how are you Contestant",4))
+console.log(truncateSentence("What is the solution to this problem",4))
+console.log(truncateSentence("chopper is not a tanuki",5))
