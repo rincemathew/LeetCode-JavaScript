87dadd83bcfb4a76cb52a33d72aa9ddb41a6ae66
Rince Mathew <rincemathewmanattu@gmail.com>
2023-04-16 22:48:07 +0530

    real answer

--- 283.js
+++ 283.js
@@ -1,3 +1,7 @@
+// Move Zeroes
+// Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+
+// Note that you must do this in-place without making a copy of the array.
 /**
  * @param {number[]} nums
  * @return {void} Do not return anything, modify nums in-place instead.
