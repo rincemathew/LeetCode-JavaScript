4a5cdfc15417cb16840462dbae1d1d175f575188
Rince Mathew <rincemathewmanattu@gmail.com>
2023-07-22 20:03:51 +0530

    goal parser interperetation

---
+++ 1678.js
@@ -0,0 +1,12 @@
+//Goal Parser Interpretation
+
+
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    return command.split("()").join("o").split("(al)").join("al");
+};
+
+console.log(interpret("G()()()()(al)"))
