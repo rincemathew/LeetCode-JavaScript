2736bc61d8440eaa6ecafc95a5fed703d155843b
Rince Mathew <rincemathewmanattu@gmail.com>
2023-08-18 15:14:01 +0530

    monotonic array

---
+++ 896.js
@@ -0,0 +1,27 @@
+// Monotonic Array
+
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var isMonotonic = function(nums) {
+    let increasing = true
+    let decreasing = true
+    if(nums.length < 3) {
+        return true
+    }
+
+    for(i=1;i<nums.length;i++) {
+        if(nums[i] > nums[i-1]) {
+            decreasing = false
+        }
+        if(nums[i] < nums[i-1]) {
+            increasing = false
+        }
+    }
+    return increasing || decreasing
+};
+
+
+console.log(isMonotonic([1,2,2,3]))
+console.log(isMonotonic([1,3,2]))
