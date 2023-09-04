58cdf597530950464129d0d1381dd0a2faa1edec
Rince Mathew <rincemathewmanattu@gmail.com>
2023-09-04 18:59:00 +0530

    Reverse Words in a String III

--- 557.js
+++ 557.js
@@ -1,10 +1,16 @@
+// Reverse Words in a String III
+
 /**
  * @param {string} s
  * @return {string}
  */
 var reverseWords = function(s) {
-    return s.split('').reverse().join('')
+    // return s.split(' ').reverse().join('')
+
+    return s.split(' ').map((word)=>{
+        return word.split('').reverse().join('')
+    }).join(' ')
 };
 
-console.log(reverseWords("Let's take LeetCode contest"))
+console.log(reverseWords("Let's take LeetCode contest"))  //"s'teL ekat edoCteeL tsetnoc"
 console.log(reverseWords("God Ding"))
