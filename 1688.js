da5898c7b2890e1ced3e83b7156ab35ce1988256
Rince Mathew <rincemathewmanattu@gmail.com>
2023-10-13 14:42:36 +0530

    some changes

--- 1688.js
+++ 1688.js
@@ -11,6 +11,18 @@ var numberOfMatches = function(n) {
         n = Math.ceil(n/2) 
     }
     return counter
+
+    // if (n === 1) {
+    //     return 0; // No matches needed when there's only one team.
+    // }
+
+    // if (n % 2 === 0) {
+    //     // If the number of teams is even, half of them advance, and the other half play matches.
+    //     return n / 2 + numberOfMatches(n / 2);
+    // } else {
+    //     // If the number of teams is odd, one team gets a bye, and the rest play matches.
+    //     return (n - 1) / 2 + numberOfMatches((n - 1) / 2 + 1);
+    // }
 };
 
 console.log(numberOfMatches(7))
