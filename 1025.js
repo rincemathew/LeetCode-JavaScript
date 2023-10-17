96373c2924f6d8e9babc7b81d1ec581490138561
Rince Mathew <rincemathewmanattu@gmail.com>
2023-10-17 10:28:09 +0530

    divisor game

---
+++ 1025.js
@@ -0,0 +1,12 @@
+// Divisor Game
+
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var divisorGame = function(n) {
+    return n%2===0
+};
+
+console.log(divisorGame(2))
+console.log(divisorGame(3))
