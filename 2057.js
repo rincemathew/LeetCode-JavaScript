d1b81af81149d82ad87076bee31ecf4e965c83e0
Rince Mathew <rincemathewmanattu@gmail.com>
2023-11-20 09:44:28 +0530

    smallest index with equal value

---
+++ 2057.js
@@ -0,0 +1,16 @@
+// Smallest Index With Equal Value
+
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var smallestEqual = function(nums) {
+    for (i = 0; i < nums.length; i++) {
+      if (i % 10 === nums[i]) {
+        return i;
+      }
+    }
+    return -1;
+};
+
+console.log(smallestEqual([4, 3, 2, 1]));
