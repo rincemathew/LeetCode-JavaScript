9b3a8a192890f580858b760f5bdd8334be88cb2b
Rince Mathew <rincemathewmanattu@gmail.com>
2024-03-26 16:43:44 +0530

    constainte space

---
+++ 137.js
@@ -0,0 +1,11 @@
+var singleNumber = function (nums) {
+  let ones = 0,
+    twos = 0;
+
+  for (let num of nums) {
+    ones = (ones ^ num) & ~twos;
+    twos = (twos ^ num) & ~ones;
+  }
+
+  return ones;
+};
