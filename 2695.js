91463058b651f89b67d8ce6d22abbc9bf3ac0e18
Rince Mathew <rincemathewmanattu@gmail.com>
2024-05-24 12:22:07 +0530

    Array Wrapper

---
+++ 2695.js
@@ -0,0 +1,16 @@
+// Array Wrapper
+
+var ArrayWrapper = function (nums) {
+  this.nums = nums;
+};
+
+/**
+ * @return {number}
+ */
+ArrayWrapper.prototype.valueOf = function () {
+  return this.nums.reduce((sum, num) => sum + num, 0);
+};
+
+ArrayWrapper.prototype.toString = function () {
+  return `[${this.nums.join(",")}]`;
+};
