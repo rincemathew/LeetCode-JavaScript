e3905dece3dce005adb963d59a5dde3e50e3f3b0
Rince Mathew <rincemathewmanattu@gmail.com>
2024-08-04 22:57:39 +0530

    2529

---
+++ 2529.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumCount = function(nums) {
+    min = 0
+    max = nums.length -1
+    while(min < max) {
+        mid = Math.ceil(min/max)/2
+
+    }
+};
+
+console.log(maximumCount([-2,-1,-1,1,2,3]))  //3
+console.log(maximumCount([-3,-2,-1,0,0,1,2])) //3
